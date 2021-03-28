2a9684ea156481a105d8372a197773800ced268c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-03-27 21:54:35 -0700

    initial explosionworker work

---
+++ Source/ExplosionWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace moretars
+{
+    public abstract class ExplosionWorker
+    {
+        public abstract void Explode(IntVec3 position, Map map, Projectile proj = null);
+    }
+    public class ExplosionWorkerExtension : DefModExtension
+    {
+        public ExplosionWorker worker;
+    }
+}
---
+++ Source/Projectile_ExplosiveWithWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace moretars
+{
+    public class Projectile_ExplosiveWithWorker : Projectile_Explosive
+    {
+        public ExplosionWorkerExtension Ext
+        {
+            get
+            {
+                if (cached) return ext;
+                cached = true;
+                ext = base.def.GetModExtension<ExplosionWorkerExtension>();
+                return ext;
+            }
+        }
+        private ExplosionWorkerExtension ext;
+        private bool cached = false;
+        public ExplosionWorker Worker => Ext.worker;
+
+        protected override void Explode()
+        {
+            Destroy();
+            Worker.Explode(base.Position, base.Map, this);
+        }
+    }
+}
